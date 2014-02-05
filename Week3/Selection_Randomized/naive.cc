bbd7cd82e531849d0ef98f8b28ec20f8b02259ba
Kaviraj <kavinofear@gmail.com>
2014-02-05 13:09:35 +0530

    Fixing naive in random selection

---
+++ Week3/Selection_Randomized/naive.cc
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<iterator>
+
+using namespace std;
+
+const string i_file="data.txt";
+const string o_file="output_naive.txt";
+
+size_t getRandom_i(size_t size);
+int choosePivot(int low,int high);
+int partition(vector<int>& v,int lower_limit,int higher_limit);
+void qsort(vector<int>& v,int lower_limit,int higher_limit);
+
+int main() {
+
+  ifstream input(i_file.c_str());
+  ofstream output(o_file.c_str());
+
+  vector<int> v;
+
+  copy(istream_iterator<int>(input),istream_iterator<int>(),back_inserter(v));
+
+  size_t i=getRandom_i(v.size());
+
+  qsort(v,0,v.size());
+
+  output<<i<<" "<<v[i-1]<<endl;
+
+  return 0;
+}
+
+size_t getRandom_i(size_t size) {
+  return (rand()%size)+1;
+}
+
+void qsort(vector<int>& v,int lower_limit,int higher_limit) {
+  if(lower_limit>=higher_limit-1)
+    return;
+  int n=higher_limit-lower_limit+1;
+  int pivot=choosePivot(lower_limit,higher_limit);
+  //  cout<<"pivot: "<<v[pivot]<<endl;
+  swap(v[lower_limit],v[pivot]);
+  int l= partition(v,lower_limit,higher_limit);
+  qsort(v,lower_limit,l);
+  qsort(v,l+1,higher_limit);
+}
+
+int partition(vector<int>& v,int lower_limit,int higher_limit) {
+
+  int i=lower_limit+1;
+  size_t n=higher_limit;
+  for(int j=lower_limit+1;j<n; ++j) {
+    if(v[j]<v[lower_limit]) {
+      swap(v[i],v[j]);
+      i++;
+    }
+  }
+  swap(v[lower_limit],v[i-1]);
+  //  copy(v.begin()+lower_limit,v.begin()+higher_limit,ostream_iterator<int>(cout,", "));
+  //  cout<<endl;
+  return i-1;
+}
+
+int choosePivot(int l,int h) {
+  return l+(rand()%(h-l));
+}
